34f4e17536ecede61bd93f37762af8dbc638b051
frozenmaple <frozenmaple@126.com>
2022-11-15 15:34:59 +0800

    double check the git on thinkbook :hammer:

--- temp_test.cpp
+++ temp_test.cpp
@@ -5,5 +5,6 @@ int main()
 	ch = getchar();
 	printf("you entered is key %c\n", ch);
 	printf("The ASCII value is %d\n", ch);
+	printf("this is the first time edited from this machine\n");
 	return 0;
 }
--- test_temp.cpp
+++ test_temp.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-  float j=85.98;
+  float j = 85.98;
   printf("%f\n", j);
   int i;
-  for (i = 0; i < 10;i++)
+  for (i = 0; i < 10; i++)
   {
     printf("_");
   }
