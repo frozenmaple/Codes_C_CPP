3ab0f1639e47655c3faabbe713a264656cee6333
Feng Qian <frozenmaple@126.com>
2022-11-16 12:12:03 +0800

    some minor changes :bookmark:

--- temp_test.cpp
+++ temp_test.cpp
@@ -3,8 +3,8 @@ int main()
 {
 	char ch;
 	ch = getchar();
-	printf("you entered is key %c\n", ch);
 	printf("The ASCII value is %d\n", ch);
 	printf("this is the first time edited from this machine\n");
+	printf("you entered is key %c\n", ch);
 	return 0;
 }
